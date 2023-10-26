e0ae8003da58daef00e8a0a7b3b5e968e8bbdae2
xu <spiridonova.kseniya@gmail.com>
2023-10-26 14:46:12 +0600

    feat: add a GET request to fetch a Pokémon and stored the search query in local storage

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,22 +1,39 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "../../styles/Search.css";
 
 interface SearchProps {}
 
 interface SearchState {
   searchValue: string;
+  searchResults: [] | null;
 }
 
 export class Search extends Component<SearchProps, SearchState> {
   constructor(props: SearchProps) {
     super(props);
     this.state = {
-      searchValue: "",
+      searchValue: localStorage.searchValue ?? "",
+      searchResults: null,
     };
   }
 
   handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({ searchValue: event.target.value });
+  };
+
+  handleSearch = () => {
+    localStorage.setItem("searchValue", this.state.searchValue);
+    console.log(localStorage);
+    axios
+      .get(`https://pokeapi.co/api/v2/berry/${this.state.searchValue}`)
+      .then((response) => {
+        this.setState({ searchResults: response.data });
+        console.log("Response:", response);
+      })
+      .catch((error) => {
+        console.error("Error while executing the GET request:", error);
+      });
   };
 
   clearInput = () => {
@@ -33,7 +50,9 @@ export class Search extends Component<SearchProps, SearchState> {
           value={this.state.searchValue}
           onChange={this.handleInputChange}
         />
-        <button className="btn search">search</button>
+        <button className="btn search" onClick={this.handleSearch}>
+          search
+        </button>
         <button className="btn search-clear" onClick={this.clearInput}>
           clear
         </button>
