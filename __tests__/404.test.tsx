b1f46f8b68d378b7e49249a8b8dfbfabf999bce0
xu <spiridonova.kseniya@gmail.com>
2023-11-28 13:06:42 +0600

    feat: init app, setup eslint, prettier and husky

--- __tests__/404.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import NotFound from '../pages/404';
-
-describe('Custom 404 Page', () => {
-  test('renders NotFound component for invalid route', () => {
-    render(<NotFound  />);
-    expect(screen.getByText('Page not found')).toBeInTheDocument();
-  });
-});
--- components/index.ts
+++
@@ -1,7 +0,0 @@
-export { Limitation } from './Limitation';
-export { Pagination } from './Pagination';
-export { SearchInput } from './SearchInput';
-export { SearchList } from './SearchList';
-export { SearchItem } from './SearchItem';
-export { Button } from './Button';
-export { ErrorBoundaryButton } from './ErrorBoundaryButton';
