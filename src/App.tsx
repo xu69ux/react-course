c2f0d573ed840a996d699c19fd784d0897171952
xu <spiridonova.kseniya@gmail.com>
2023-11-30 18:42:56 +0600

    refactor: extract nav into a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UncontrolledForm from './components/UncontrolledForm';
 import HookForm from './components/HookForm';
 import MainPage from './components/MainPage';
+import Navigation from './components/Navigation';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
@@ -9,26 +10,14 @@ function App() {
   return (
     <Provider store={store}>
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Main Page</Link>
-              </li>
-              <li>
-                <Link to="/uncontrolled-form">Uncontrolled Form</Link>
-              </li>
-              <li>
-                <Link to="/hook-form">Hook Form</Link>
-              </li>
-            </ul>
-          </nav>
+        <>
+          <Navigation />
           <Routes>
             <Route path="/uncontrolled-form" element={<UncontrolledForm />} />
             <Route path="/hook-form" element={<HookForm />} />
             <Route path="/" element={<MainPage />} />
           </Routes>
-        </div>
+        </>
       </Router>
     </Provider>
   );
